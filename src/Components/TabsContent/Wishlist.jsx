fc72ccc18226fda95912aae02ae1c0c1bb20f347
Coding Hub <n.jm90@yahoo.com>
2024-08-06 17:14:07 +0300

    wishlist basic changes

--- src/Components/TabsContent/Wishlist.jsx
+++ src/Components/TabsContent/Wishlist.jsx
@@ -17,38 +17,46 @@ const Wishlist = () => {
   const toggleOpen = () => setOpen((prev) => !prev);
 
   return (
-    <>
-      <Tabs value="Pending" className="grid justify-center">
-        <TabsHeader className="bg-gray-300 w-64">
-          <Tab key={"Pending"} value="Pending">
-            Pending
-          </Tab>
-          <Tab key={"Done"} value="Done">
-            Done
-          </Tab>
-        </TabsHeader>
-        <TabsBody className="">
-          <TabPanel key={"Pending"} value="Pending">
-            Pending
-          </TabPanel>
-          <TabPanel key={"Done"} value="Done">
-            Done
-          </TabPanel>
-        </TabsBody>
+    <div className="max-w-[450px]">
+      <Tabs value="Pending" className="">
+        <div>
+          <TabsHeader className="bg-gray-300 w-64">
+            <Tab key={"Pending"} value="Pending">
+              Pending
+            </Tab>
+            <Tab key={"Done"} value="Done">
+              Done
+            </Tab>
+          </TabsHeader>
+        </div>
+        <div>
+          <TabsBody className="">
+            <TabPanel key={"Pending"} value="Pending">
+              <p>Pending</p>
+              <div
+                onClick={toggleOpen}
+                className="border border-gray-400 bg-gray-100 w-full rounded-md p-2"
+              >
+                Open Collapse
+                <Collapse open={open}>
+                  <Card className="w-full rounded-tl-none rounded-tr-none">
+                    <CardBody className="p-2 rounded-tl-none">
+                      <div>
+                        Use our Tailwind CSS collapse for your website. You can
+                        use if for accordion, collapsible items and much more
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Collapse>
+              </div>
+            </TabPanel>
+            <TabPanel key={"Done"} value="Done">
+              Done
+            </TabPanel>
+          </TabsBody>
+        </div>
       </Tabs>
-
-      <Button onClick={toggleOpen}>Open Collapse</Button>
-      <Collapse open={open}>
-        <Card className="my-4 mx-auto w-8/12">
-          <CardBody>
-            <Typography>
-              Use our Tailwind CSS collapse for your website. You can use if for
-              accordion, collapsible items and much more.
-            </Typography>
-          </CardBody>
-        </Card>
-      </Collapse>
-    </>
+    </div>
   );
 };
 
