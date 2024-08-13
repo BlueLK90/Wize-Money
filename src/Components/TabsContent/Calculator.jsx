e257893bb2aa94769779402bde64febbaca6b1df
Coding Hub <n.jm90@yahoo.com>
2024-08-13 11:57:47 +0300

    calc done but it can be improved

--- src/Components/TabsContent/Calculator.jsx
+++ src/Components/TabsContent/Calculator.jsx
@@ -1,63 +1,77 @@
 import { useState } from "react";
 import { FiDelete } from "react-icons/fi";
+import { numberWithCommas } from "../../Utils";
 
 const btnStyle =
-  "flex justify-center items-center h-12 w-12 p-4 shadow-sm shadow-gray-400 rounded-full text-xs md:text-sm lg:text-base font-medium text-darkapricot";
+  "flex justify-center items-center h-12 w-12 p-4 shadow-sm shadow-gray-400 rounded-full text-base font-medium text-darkapricot hover:opacity-70";
 
 const Calculator = () => {
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
+  //const [reset, setReset] = useState(false); //startover
+  const [calculating, setCalculating] = useState(""); //manage the sizes of input screens
 
-  var calSignClicked;
-  const handleClickNum = (val) => {
-    if (input === 0 || calSignClicked === true) {
-      setInput(val);
-      calSignClicked = false;
-    } else {
-      setInput((prev) => prev + val);
+  const mathSigns = ["+", "-", "*", "/", "."];
+
+  const handleClick = (val) => {
+    if (mathSigns.includes(val) && input === "") {
+      return;
     }
-  };
-  const handleClickSign = (val) => {
-    calSignClicked = true;
-    setResult((prev) => {
-      if (prev.slice(-1) !== val) {
-        return prev.slice(0, -1) + val;
-      } else {
-        return prev + input + val;
-      }
-    });
-  };
 
-  const backSpace = () => {
-    setInput(input.slice(0, -1));
-  };
+    if (mathSigns.includes(val) && mathSigns.includes(input.slice(-1))) {
+      setCalculating(calculating.slice(0, -1) + val);
+      setInput(input.slice(0, -1) + val);
+    } else {
+      setCalculating(calculating + val);
+      setInput(input + val);
+    }
 
-  const clearResult = () => {
-    setInput("");
-    setResult("");
-  };
+    if (!mathSigns.includes(val)) {
+      setResult(eval(input + val));
+    }
+  }; // +-*/ btn
 
   const calculate = () => {
     try {
-      setInput(eval(result));
-      setResult(result + "=");
+      if (mathSigns.includes(input.slice(-1))) {
+        setCalculating(calculating.slice(0, -1) + "=");
+        setInput(eval(input.slice(0, -1)));
+      } else {
+        setCalculating(calculating + " =");
+        setInput(eval(input));
+        setResult("");
+      }
     } catch (error) {
       setResult("Error");
     }
-  };
+  }; // = btn
+
+  const backSpace = () => {
+    setCalculating(calculating.slice(0, -1));
+    setInput(input.slice(0, -1));
+  }; //backspace btn
+
+  const clearResult = () => {
+    setCalculating("");
+    setInput("");
+    setResult("");
+  }; //clear all btn
 
   return (
     <div className="bg-gray-100 w-[95%] lg:w-[80%] mx-auto pb-4 rounded-md">
       <div className="bg-transparent px-4 pt-4 w-full max-w-xs sm:max-w-md md:max-w-lg">
         {/* screens div */}
         <div className="mb-1">
-          <div className="text-right w-full h-40 xl:h-24 p-2 overflow-auto text-xl mt-2 border border-b-0 border-gray-300 rounded">
-            {result}
+          <div className="bg-white/50 border border-b-0 border-gray-300 rounded p-2 mt-2 text-xl text-right w-full h-40 xl:h-24 overflow-auto">
+            <p className="h-[75%] text-green-500">{numberWithCommas(result)}</p>
+            <p className="text-sm text-green-300">
+              {numberWithCommas(calculating)}
+            </p>
           </div>
           <input
             type="text"
-            className="w-full p-2 text-right text-2xl border border-gray-300 rounded"
-            value={input}
+            className="w-full p-2 text-right text-2xl text-green-700 border border-gray-300 rounded"
+            value={numberWithCommas(input) || "0"}
             readOnly
           />
         </div>
@@ -86,7 +100,7 @@ const Calculator = () => {
             <button
               key={"/"}
               className={btnStyle}
-              onClick={() => handleClickSign("/")}
+              onClick={() => handleClick("/")}
             >
               /
             </button>
@@ -94,9 +108,7 @@ const Calculator = () => {
               <button
                 key={value}
                 className={btnStyle}
-                onClick={() =>
-                  value === "*" ? handleClickSign(value) : handleClickNum(value)
-                }
+                onClick={() => handleClick(value)}
               >
                 {value}
               </button>
@@ -105,9 +117,7 @@ const Calculator = () => {
               <button
                 key={value}
                 className={btnStyle}
-                onClick={() =>
-                  value === "-" ? handleClickSign(value) : handleClickNum(value)
-                }
+                onClick={() => handleClick(value)}
               >
                 {value}
               </button>
@@ -116,9 +126,7 @@ const Calculator = () => {
               <button
                 key={value}
                 className={btnStyle}
-                onClick={() =>
-                  value === "+" ? handleClickSign(value) : handleClickNum(value)
-                }
+                onClick={() => handleClick(value)}
               >
                 {value}
               </button>
@@ -134,7 +142,7 @@ const Calculator = () => {
                     : ""
                 }`}
                 onClick={() =>
-                  value === "=" ? calculate() : handleClickNum(value)
+                  value === "=" ? calculate() : handleClick(value)
                 }
               >
                 {value}
